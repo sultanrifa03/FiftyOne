e5f4a94db76342aea5e448e79947d9d3a44452b6
Sultan Syekh Nuhly Rifa Rizkya Robby Hidayatullah <135625424+sultanrifa03@users.noreply.github.com>
2023-11-22 08:31:06 +0800

    Update contact.jsx
    sdh membuat contact

--- src/pages/landing-page/contact.jsx
+++ src/pages/landing-page/contact.jsx
@@ -1,6 +1,163 @@
 import React from "react";
 
 export const Contact = () => {
-  return <div>Contact</div>;
-};
+  return (
+    <>
+    <div className="Kontak w-96 h-96 relative bg-white">
+      <div className="HomePage w-96 h-96 left-[-20px] top-[65px] absolute bg-white">
+        <div className="Rectangle1 w-96 h-96 left-0 top-0 absolute bg-zinc-300" />
+        <div className="Ellipse1 w-72 h-72 left-0 top-0 absolute bg-teal-700 bg-opacity-50 rounded-full blur-3xl" />
+        <div className="Ellipse2 w-72 h-72 left-[664px] top-[266px] absolute bg-teal-700 bg-opacity-50 rounded-full blur-3xl" />
+        <div className="Ellipse3 w-72 h-72 left-[185px] top-[491px] absolute bg-orange-300 bg-opacity-50 rounded-full blur-3xl" />
+        <div className="Ellipse4 w-72 h-72 left-[1200px] top-[416px] absolute bg-orange-300 bg-opacity-50 rounded-full blur-3xl" />
+        <img className="Rectangle w-96 h-96 left-0 top-0 absolute" src="https://via.placeholder.com/1512x982" />
+        <img className="PlanetEarthWithPlantLogoDesignVector29145444RemovebgPreview1 w-40 h-32 left-[76px] top-0 absolute" src="https://via.placeholder.com/157x136" />
+        <img className="LeafPlantLeavesFlowerPngFavpngW8nipqstkckgwxfnfhwap4byaRemovebgPreview2 w-96 h-96 left-[927px] top-[336.80px] absolute origin-top-left rotate-[-20deg] opacity-50 blur-3xl" src="https://via.placeholder.com/552x452" />
+        <img className="LeafPlantLeavesFlowerPngFavpngW8nipqstkckgwxfnfhwap4byaRemovebgPreview1 w-96 h-96 left-[995px] top-[197px] absolute" src="https://via.placeholder.com/552x452" />
+        <img className="Istockphoto1180581523612x612RemovebgPreview2 w-96 h-96 left-[919.12px] top-[553px] absolute origin-top-left rotate-[20deg] opacity-50 blur-3xl" src="https://via.placeholder.com/572x436" />
+        <img className="Istockphoto1180581523612x612RemovebgPreview1 w-96 h-96 left-[761px] top-[573px] absolute" src="https://via.placeholder.com/572x436" />
+        <div className="Group1000001753 w-96 h-96 left-[185px] top-[114px] absolute">
+          <div className="Rectangle2 w-[70vh] h-[90vh] bg-[#002c02] left- top-0 absolute bg-zinc-300 rounded-lg" />
+          <div className="KotakInformasi-no-wrap w-[40vh] left-[47px] top-[60px] absolute text-4xl font-bold text-white font-['Poppins']">Kotak Informasi</div>
+          <div className="KotakInformasi-no-wrap w-[40vh] left-[47px] top-[100px] absolute text-1xl font-normal text-white font-['Poppins']">Katakan sesuatu untuk memulai obrolan langsung! </div>
+          <div className="+1012 3456 789 w-96 left-[20vh] top-[35vh] absolute text-1xl font-normal text-white font-['Poppins']">+1012 3456 789 </div>
+          <div class="Ellipse3 w-10 h-10 left-[10vh] top-[75vh] absolute bg-green-600 rounded-full"></div>
+          <div class="Ellipse3 w-10 h-10 left-[20vh] top-[75vh] absolute bg-white rounded-full"></div>
+          <div class="Ellipse3 w-10 h-10 left-[30vh] top-[75vh] absolute bg-green-600 rounded-full"></div>
+          <div className="ForEat@gmail.com w-96 left-[20vh] top-[44vh] absolute text-1xl font-normal text-white font-['Poppins']">ForEat@gmail.com </div>
+          <div className="+1012 3456 789 w-[40vh] left-[20vh] top-[54vh] absolute text-1xl font-normal text-white font-['Poppins']">132 Dartmouth Street Bostom, Massacusetts 02156 Sunda Pride </div>
+          <div className="Rectangle3842 w-[160vh] h-[90vh] bg-[#C8C8C8] left-0 top-0 rounded-lg" />
+          <div className="Bannar w-96 h-96 left-[342px] top-0 absolute rounded-lg">
+           <img className="Bg w-96 h-96 left-[680px] top-0 absolute origin-top-left rotate-180 rounded-lg" src="https://via.placeholder.com/680x652" />
+          </div>
+          <div className="ContactFild w-96 h-96 left-[535px] top-[61px] absolute">
+          <div className="Rectangle6 w-[70vh] h-16 left-16 top-0 absolute bg-neutral-400 bg-opacity-95 rounded-lg" />
+          <div className="Nama w-12 left-[87px] top-[20px] absolute text-center text-black text-opacity-50 text-lg font-normal font-['Poppins']">Nama</div>
+            <div className="Rectangle8 w-[70vh] h-16 left-16 top-[116px] absolute bg-neutral-400 bg-opacity-95 rounded-lg" />
+            <div className="Email w-12 left-[87px] top-[135px] absolute text-center text-black text-opacity-50 text-lg font-normal font-['Poppins']">Email</div>
+            <div className="Rectangle7 w-[70vh] h-52 left-16 top-[232px] absolute bg-neutral-400 bg-opacity-95 rounded-lg" />
+            <div className="TuliskanSesuatu w-44 left-[87px] top-[255px] absolute text-black text-opacity-50 text-lg font-normal font-['Poppins']">Tuliskan sesuatu...</div>
+            <div className="Frame w-48 h-14 px-12 py-3.5 left-[225px] top-[476px] absolute bg-green-950 rounded-lg shadow justify-start items-start gap-2.5 inline-flex">
+            <div className="KirimPesan text-center text-white text-base font-medium font-['Poppins']">Kirim Pesan</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Group79 w-96 h-96 left-[-5px] top-[921px] absolute">
+        <div className="Rectangle61 w-[205vh] h-96 left-0 top-[12px] absolute bg-zinc-900" />
+        <div className="MaskGroup w-80 h-96 left-[2px] top-[12px] absolute">
+          <div className="Bg w-80 h-96 left-0 top-0 absolute bg-zinc-900" />
+          <div className="Group origin-top-left rotate-[-92.35deg] opacity-60 w-96 h-96 left-[-705.44px] top-[372.69px] absolute">
+            <img className="904 w-12 h-16 left-[821.94px] top-[-171.69px] absolute origin-top-left rotate-[30deg]" src="https://via.placeholder.com/48x71" />
+          </div>
+        </div>
+        <div className="Group76 origin-top-left rotate-[-24.61deg] w-36 h-28 left-[1254px] top-[62.06px] absolute">
+        </div>
+        <div className="Group77 origin-top-left rotate-[-1.75deg] w-36 h-20 left-[1290px] top-[164.55px] absolute">
+        </div>
+        <div className="Group78 origin-top-left -rotate-90 w-36 h-20 left-[1325px] top-[417.25px] absolute">
+        </div>
+        <div className="DownloadMobileApp w-80 h-28 left-[975px] top-[121px] absolute flex-col justify-start items-start gap-3 inline-flex">
+          <div className="SegeraUnduhAplikasinya text-white text-lg font-medium font-['Poppins'] leading-relaxed">Segera Unduh Aplikasinya</div>
+          <div className="DownloadOurMobileApp pt-2 justify-start items-start gap-2 inline-flex">
+            <div className="AppStore p-2.5 bg-zinc-800 rounded justify-start items-center gap-1.5 flex">
+              <div className="AppleNegative1 w-7 h-7 relative" />
+              <div className="Frame145 flex-col justify-start items-start inline-flex">
+                <div className="DownloadOnThe w-34 text-zinc-400 text-xs font-normal font-['Poppins'] leading-none">Download on the</div>
+                <div className="AppStore w-24 h-6 text-white text-base font-medium font-['Poppins'] leading-normal">App Store</div>
+              </div>
+            </div>
+            <div className="GooglePlay p-2.5 bg-zinc-800 rounded justify-start items-center gap-1.5 flex">
+              <div className="IconGooglePlay1 w-6 h-6 relative" />
+              <div className="Frame145 flex-col justify-start items-start inline-flex">
+                <div className="DownloadOnThe w-34 text-zinc-400 text-xs font-normal font-['Poppins'] leading-none">Download on the</div>
+                <div className="GooglePlay w-34 h-6 text-white text-base font-medium font-['Poppins'] leading-normal">Google play</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Copyright w-[205vh] h-20 bg-[#18181B] px-72 py-6 left-0 top-[348px] absolute bg-zinc-900 shadow border" />
+        <div className="ForEatEcommerce2023AllRightsReserved-no-wrap w-96 left-[30vh] top-[380px] absolute text-zinc-500 text-sm font-normal font-['Poppins'] leading-tight">For Eat eCommerce © 2023 All Rights Reserved</div>
+        <div className="Frame142 w-72 h-8 left-[998px] top-[373px] absolute justify-start items-start gap-2 inline-flex">
+          <div className="MethodApplepay w-11 h-8 relative">
+            <div className="PaymentMethod w-11 h-8 left-0 top-0 absolute">
+              <div className="Base w-11 h-8 left-0 top-0 absolute bg-zinc-900 rounded-md border border-zinc-800" />
+            </div>
+            <img className="Applepay w-8 h-3.5 left-[6.62px] top-[9.26px] absolute" src="https://via.placeholder.com/32x14" />
+          </div>
+          <div className="MethodVisa w-11 h-8 relative">
+            <div className="PaymentMethod w-11 h-8 left-0 top-0 absolute">
+              <div className="Base w-11 h-8 left-0 top-0 absolute bg-zinc-900 rounded-md border border-zinc-800" />
+            </div>
+            <img className="VisaLogo w-8 h-2.5 left-[6.35px] top-[10.72px] absolute" src="https://via.placeholder.com/31x10" />
+          </div>
+          <div className="MethodDiscover w-11 h-8 relative">
+            <div className="PaymentMethod w-11 h-8 left-0 top-0 absolute">
+              <div className="Base w-11 h-8 left-0 top-0 absolute bg-zinc-900 rounded-md border border-zinc-800" />
+            </div>
+            <div className="Discover w-10 h-5 left-[3.97px] top-[11.91px] absolute">
+              <img className="DiscVer w-9 h-1.5 left-0 top-0 absolute" src="https://via.placeholder.com/38x7" />
+            </div>
+          </div>
+          <div className="MethodMastercard w-11 h-8 relative">
+            <div className="PaymentMethod w-11 h-8 left-0 top-0 absolute">
+              <div className="Base w-11 h-8 left-0 top-0 absolute bg-zinc-900 rounded-md border border-zinc-800" />
+            </div>
+            <div className="Mastercard w-7 h-4 left-[7.94px] top-[6.66px] absolute">
+              <img className="Left w-7 h-4 left-0 top-0 absolute" src="https://via.placeholder.com/30x18" />
+              <img className="Right w-3.5 h-4 left-[14.80px] top-0 absolute" src="https://via.placeholder.com/15x18" />
+              <img className="Middle w-1.5 h-3.5 left-[11.64px] top-[2.12px] absolute" src="https://via.placeholder.com/6x13" />
+            </div>
+          </div>
+          <div className="Cart w-16 h-8 relative">
+            <div className="Base w-16 h-8 left-0 top-0 absolute bg-zinc-900 rounded-md border border-zinc-800" />
+            <div className="Lock1 w-2.5 h-2.5 left-[5px] top-[4px] absolute bg-zinc-900">
+              <div className="Group w-2 h-2.5 left-[1.38px] top-[0.92px] absolute">
+              </div>
+            </div>
+            <div className="Secure left-[18px] top-[4px] absolute text-white text-xs font-normal font-['Poppins'] leading-3">Secure</div>
+            <div className="Payment w-16 left-0 top-[16px] absolute text-center text-white text-xs font-semibold font-['Poppins'] leading-3">Payment</div>
+          </div>
+        </div>
+        <div className="MyAccount w-28 h-10 left-[642px] top-[121px] absolute flex-col justify-start items-start gap-3 inline-flex">
+          <div className="KontakKami w-[30vh] text-white text-lg font-medium font-['Poppins'] leading-relaxed">Kontak Kami</div>
+        </div>
+        <div className="Kantor0210012345678Mobile628123456EmailForeatsGmailCom w-72 h-28 left-[642px] top-[172px] absolute text-neutral-400 text-base font-normal font-['Poppins'] leading-normal">Kantor : (021) 001 2345678<br/>Mobile : +62 812 3456<br/>Email   : ForEats@gmail.com</div>
+        <div className="Company w-72 h-64 left-[173px] top-[69.50px] absolute flex-col justify-start items-start gap-4 inline-flex">
+          <img className="B711221A8404791864aFb051794e8ed1 w-36 h-20" src="https://via.placeholder.com/139x86" />
+          <div className="SebuahAplikasiYangMemudahkanSemuaOrangUntukBerbelanjaKebutuhanSehariHariDenganCepatDanMudah w-72 text-neutral-400 text-base font-normal font-['Poppins'] leading-normal">Sebuah aplikasi yang memudahkan semua orang untuk berbelanja kebutuhan sehari hari dengan cepat dan mudah</div>
+          <div className="SocialMedia justify-start items-start gap-2 inline-flex">
+            <div className="SocialMedia w-10 h-10 relative">
+              <div className="Ellipse4 w-10 h-10 left-0 top-0 absolute bg-green-600 rounded-full" />
+              <div className="Facebook1 w-4 h-4 left-[11px] top-[11px] absolute" />
+            </div>
+            <div className="SocialMedia w-10 h-10 relative bg-white rounded-full">
+              <div className="Ellipse5 w-10 h-10 left-0 top-0 absolute rounded-full" />
+              <div className="Twitter1 w-4 h-4 left-[11px] top-[11px] absolute" />
+            </div>
+            <div className="SocialMedia w-10 h-10 relative bg-white rounded-full">
+              <div className="Ellipse6 w-10 h-10 left-0 top-0 absolute rounded-full" />
+              <div className="Pinterest1 w-4 h-4 left-[11px] top-[11px] absolute" />
+            </div>
+            <div className="SocialMedia w-10 h-10 relative bg-white rounded-full">
+              <div className="Ellipse3 w-10 h-10 left-0 top-0 absolute rounded-full" />
+              <div className="Instagram1 w-4 h-4 left-[11px] top-[11px] absolute" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Group76 fixed w-96 h-16 left-[-2px] top-0">
+        <div className="Rectangle60 w-[205vh] h-16 left-0 top-[1px] absolute bg-green-950" />
+        <div className="Group74 w-96 h-7 left-[205px] top-[17px] absolute">
+          <div className="Beranda left-0 top-0 absolute text-center text-white text-xl font-bold font-['Poppins'] leading-7">Beranda</div>
+          <div className="TentangKami left-[142px] top-0 absolute text-center text-white text-xl font-bold font-['Poppins'] leading-7">Tentang Kami </div>
+          <div className="Kontak left-[346px] top-0 absolute text-center text-white text-xl font-bold font-['Poppins'] leading-7">Kontak</div>
+        </div>
+        <img className="Rectangle59 w-24 h-16 left-[47px] top-0 absolute" src="https://via.placeholder.com/100x62" />
+      </div>
+    </div>
+    
+    </>
+  )
+  };
 export default Contact;
